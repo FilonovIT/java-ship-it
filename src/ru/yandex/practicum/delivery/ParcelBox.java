5cabf6398e04794d26523408f5420923cd3f47f5
FilonovIT <filonovIT@yandex.ru>
2026-08-18 11:15:49 +0400

    Update ParcelBox.java

--- src/ru/yandex/practicum/delivery/ParcelBox.java
+++ src/ru/yandex/practicum/delivery/ParcelBox.java
@@ -1,3 +1,23 @@
 public class ParcelBox<T extends Parcel> {
+  private static final List<T> listT = new ArrayList<>();
+  private int weightMax; 
+
+  public ParcelBox(int weightMax) {
+    this.weightMax = weightMax;
+  }
   
+  public void addParcel(T value) {
+    if ((weightMax - value.weight) == 0 || (weightMax - value.weight) > value.weight) {
+      listT.add(value);
+      weightMax -= value.weight;
+    } else {
+      System.out.println("Create new box!");
+    }
+  }
+
+  public void getAllParcels(List<T> listT) {
+    for (T element : listT) {
+      System.out.println(element);
+    }
+  }
 }
